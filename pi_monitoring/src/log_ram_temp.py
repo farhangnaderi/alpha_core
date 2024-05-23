563508ce742796dd4a116b856d41ae05df0399f3
tjsos <tony.jacob@uri.edu>
2024-05-23 13:58:19 -0400

    feat:cpu usage & temp msg type

--- pi_monitoring/src/log_ram_temp.py
+++ pi_monitoring/src/log_ram_temp.py
@@ -9,13 +9,16 @@
 
 import os
 import rospy
-import time
+import psutil
 from std_msgs.msg import Float64
+from sensor_msgs.msg import Temperature
 
 class Log_RAM_Temp:
     def __init__(self) -> None:
         self.pub_ram_info = rospy.Publisher("/alpha_rise/pi/ram_utilized", Float64, queue_size=1)
-        self.pub_temp_info = rospy.Publisher("/alpha_rise/pi/cpu_temp", Float64, queue_size=1)
+        self.pub_cpu_temp_info = rospy.Publisher("/alpha_rise/pi/cpu/temp", Temperature, queue_size=1)
+        self.pub_cpu_usage_info = rospy.Publisher("/alpha_rise/pi/cpu/utilized", Float64, queue_size=1)
+
         rospy.loginfo("Pi Monitoring started")
         self.rate = rospy.Rate(1)
         self.collect_and_publish()
@@ -35,14 +38,24 @@ class Log_RAM_Temp:
         mem_used = mem_total - mem_available
         return mem_total, mem_used
     
+    def get_cpu_usage(self):
+        return psutil.cpu_percent()
+    
     def collect_and_publish(self):
+        temp_msg = Temperature()
         while not rospy.is_shutdown():
+            #Temperature
             temp = self.get_cpu_temp()
+            temp_msg.header.stamp = rospy.Time()
+            temp_msg.temperature = temp
+            #RAM
             ram_total, ram_used = self.get_ram_usage()
-            # print([temp,ram_used/ 1048576.0])
-            #kB to GB
+            #CPU
+            cpu_used = self.get_cpu_usage()
+
             self.pub_ram_info.publish((ram_used/ram_total) *100)
-            self.pub_temp_info.publish(temp)
+            self.pub_cpu_temp_info.publish(temp)
+            self.pub_cpu_usage_info.publish(cpu_used)
             self.rate.sleep()
     
 if __name__ == "__main__":
